4eb496e6641fc9596c45ff439ec7d5c1219f83cb
KR0WD <138092079+Kr0wd@users.noreply.github.com>
2024-11-13 04:00:01 +0530

    Add files via upload

---
+++ macro.c
@@ -0,0 +1,130 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char label[10], opcode[10], operand[100];
+char args[10][10];
+int expanding = 0;
+long int strt,end;
+
+FILE *deftab, *argtab, *namtab, *out, *input;
+
+void expand();
+void define();
+
+void build_arg_list(){
+    int p = 0 ;
+    char *token = strtok(operand,",'");
+    while(token != NULL){
+        strcpy(args[++p],token);
+        token = strtok(NULL,",'");
+    }
+}
+
+void replace_args(){
+    char new_op[100], arg[10],temp[100];
+    int k = 0;
+    for (int i = 0; i < strlen(operand); i++){
+        if(operand[i] == '&'){
+            strcpy(temp,operand + i);
+            char *token = strtok(temp,"',");
+            int p = 0;
+            while(p < 10){
+                if(strcmp(args[p],token) == 0){
+                    new_op[k++] = '?';
+                    new_op[k++] = p + 48;
+                    i += strlen(token);
+                    break;
+                }
+                p++;
+            }
+            if(i < strlen(operand))
+                new_op[k++] = operand[i];
+        }
+        else if(operand[i] == '?'){
+            int j = 0, pos = operand[++i] - 48;
+            while(j < strlen(args[pos]))
+                new_op[k++] = args[pos][j++];
+        }
+        else
+            new_op[k++] = operand[i];
+    }
+    new_op[k] = '\0';
+    strcpy(operand,new_op);
+}
+
+void process_line(){
+    int flag = 0;
+    char m_name[10];
+    fseek(namtab,0,SEEK_SET);
+    while (fscanf(namtab, "%s\t%ld\t%ld", m_name, &strt, &end) != EOF) {
+        if (strcmp(opcode, m_name) == 0) {
+            flag = 1;
+            break;
+        }
+    }
+    if(flag)
+        expand();
+    else if(strcmp(opcode,"MACRO") == 0)
+        define();
+    else if(strcmp(opcode,"MEND") != 0)
+        fprintf(out,"%s\t%s\t%s\n",label,opcode,operand);
+}
+
+void get_line(){
+    if(expanding){
+        fscanf(deftab,"%s\t%s\t%s",label,opcode,operand);
+        replace_args();
+    }
+    else
+        fscanf(input,"%s\t%s\t%s",label,opcode,operand);
+}
+
+void expand(){
+    expanding = 1;
+    fprintf(out,".%s\t%s\t%s\n",label,opcode,operand);
+    build_arg_list();
+    fseek(deftab,strt,SEEK_SET);
+    get_line();
+    while(strcmp(opcode,"MEND") != 0){
+        get_line();
+        process_line();
+    }
+    expanding = 0;
+    fseek(deftab,0,SEEK_END);
+}
+
+void define(){
+    int pos = 0, j;
+    char copylabel[10];
+    strt = ftell(deftab);
+    fprintf(deftab,"%s\t%s\t%s\n",label,opcode,operand);
+    strcpy(copylabel,label);
+    build_arg_list();
+    int level = 1;
+    while(level > 0){
+        get_line();
+        replace_args();
+        fprintf(deftab,"%s\t%s\t%s\n",label,opcode,operand);
+        if(strcmp(opcode,"MACRO") == 0)
+            level++;
+        else if(strcmp(opcode,"MEND") == 0)
+            level--;
+    }
+    end = ftell(deftab);
+    fseek(namtab,0,SEEK_END);
+    fprintf(namtab,"%s\t%ld\t%ld\n",copylabel,strt,end);
+}
+
+int main(){
+    input = fopen("input.txt", "r");
+    deftab = fopen("deftab.txt", "w+");
+    namtab = fopen("namtab.txt", "w+");
+    out = fopen("out.txt", "w");
+    if(input != NULL){
+        while (strcmp(opcode,"END") != 0){
+            get_line();
+            process_line();
+        }
+    }
+}
